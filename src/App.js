f60ca08eb1d60cbe314d47d6175e7b9a55b39cec
MrDogukan <aliseslikaya@gmail.com>
2021-12-11 16:06:50 +0300

    router commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Clarusway</h1>
+      <main>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </main>
     </div>
   );
 }
